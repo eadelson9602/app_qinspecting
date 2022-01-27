593b1d904a15bd9e32055a64a878a648b9c4fcee
Elver Rodriguez <adelson9602@gmail.com>
2022-01-26 22:57:54 -0500

    se cambia path de importación de provider

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:provider/provider.dart';
 
 import 'screens/screens.dart';
 
-import 'package:app_qinspecting/providers/ui_provider.dart';
+import 'package:app_qinspecting/providers/providers.dart';
 
 void main() {
   runApp(const MyApp());
