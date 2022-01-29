6b812bd1841e4f1dc0f8d551e43e85d7b57f4c5a
Elver Rodriguez <adelson9602@gmail.com>
2022-01-28 20:54:08 -0500

    Se exporta widget appBar

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,3 +1,4 @@
 export 'package:app_qinspecting/widgets/auth_background.dart';
+export 'package:app_qinspecting/widgets/custom_app_bar.dart';
 export 'package:app_qinspecting/widgets/custom_drawer.dart';
 export 'package:app_qinspecting/widgets/custom_navigatorbar.dart';
