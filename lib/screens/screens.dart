39a39749f9fa10a0e377ebbb49356cbb6f2008ec
Elver Rodriguez <adelson9602@gmail.com>
2022-01-28 20:57:13 -0500

    Se exporta screen profile

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -3,3 +3,4 @@ export 'package:app_qinspecting/screens/home_screen.dart';
 export 'package:app_qinspecting/screens/inspection_screen.dart';
 export 'package:app_qinspecting/screens/login_screen.dart';
 export 'package:app_qinspecting/screens/signature_screen.dart';
+export 'package:app_qinspecting/screens/profile_screen.dart';
