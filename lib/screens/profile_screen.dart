573d512866928e368923a2d86befe9740f8a39d7
Elver Rodriguez <adelson9602@gmail.com>
2022-01-28 20:56:14 -0500

    Se importa custom bar y custom drawer

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'package:app_qinspecting/widgets/widgets.dart';
+
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: const CustomAppBar().createAppBar(),
+      drawer: const CustomDrawer(),
       body: Text('Pefil del usuario'),
     );
   }
