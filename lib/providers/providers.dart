b990807b8c4bf89d44d5dbf545259e9ac9087b2e
Elver Rodriguez <adelson9602@gmail.com>
2022-01-31 11:03:34 -0500

    se exporta el provider inspección

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,2 +1,3 @@
 export 'package:app_qinspecting/providers/ui_provider.dart';
 export 'package:app_qinspecting/providers/login_form_provider.dart';
+export 'package:app_qinspecting/providers/inspeccion_provider.dart';
