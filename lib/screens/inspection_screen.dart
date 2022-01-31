1030db27d9631091a893f3db987a327ff2e20678
Elver Rodriguez <adelson9602@gmail.com>
2022-01-31 16:04:19 -0500

    Se agrega campos faltantes para realizar la inspección

--- lib/screens/inspection_screen.dart
+++ lib/screens/inspection_screen.dart
@@ -1,17 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:app_qinspecting/ui/input_decorations.dart';
-
-enum SiNo { si, no }
+import 'package:app_qinspecting/providers/providers.dart';
 
 class InspectionScreen extends StatelessWidget {
   const InspectionScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    SiNo? _optSelected = SiNo.si;
-    return Container(
-      child: SingleChildScrollView(
+    final inspeccionForm = Provider.of<InspeccionProvider>(context);
+
+    Widget guiaTransporte() {
+      return inspeccionForm.tieneGuia
+          ? Column(
+              children: [
+                TextFormField(
+                  autocorrect: false,
+                  keyboardType: TextInputType.text,
+                  validator: (value) {
+                    if (value!.isEmpty) return 'Ingrese guía transporte';
+                    return null;
+                  },
+                  decoration: InputDecorations.authInputDecorations(
+                      hintText: '',
+                      labelText: 'Guía transporte',
+                      prefixIcon: Icons.speed),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                if (inspeccionForm.tieneGuia)
+                  TextFormField(
+                    autocorrect: false,
+                    keyboardType: TextInputType.text,
+                    validator: (value) {
+                      if (value!.isEmpty)
+                        return 'Ingrese foto de la guía transporte';
+                      return null;
+                    },
+                    decoration: InputDecorations.authInputDecorations(
+                        hintText: '',
+                        labelText: 'Foto de la guía transporte',
+                        prefixIcon: Icons.speed),
+                  ),
+              ],
+            )
+          : Container();
+    }
+
+    Widget infoRemolque() {
+      return Column(children: [
+        DropdownButtonFormField(
+            decoration: InputDecorations.authInputDecorations(
+                prefixIcon: Icons.local_shipping,
+                hintText: '',
+                labelText: 'Placa del remolque'),
+            items: const [DropdownMenuItem(value: 1, child: Text('Placa 1'))],
+            onChanged: (value) {
+              print(value);
+            }),
+        const SizedBox(
+          height: 10,
+        ),
+        TextFormField(
+          textCapitalization: TextCapitalization.words,
+          autocorrect: false,
+          readOnly: true,
+          keyboardType: TextInputType.text,
+          decoration: InputDecorations.authInputDecorations(
+              hintText: '', labelText: 'Marca del remolque'),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        TextFormField(
+          textCapitalization: TextCapitalization.words,
+          autocorrect: false,
+          readOnly: true,
+          keyboardType: TextInputType.text,
+          decoration: InputDecorations.authInputDecorations(
+              hintText: '', labelText: 'Modelo del remolque'),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        TextFormField(
+          textCapitalization: TextCapitalization.words,
+          autocorrect: false,
+          readOnly: true,
+          keyboardType: TextInputType.text,
+          decoration: InputDecorations.authInputDecorations(
+              hintText: '', labelText: 'Licencia de tránsito del remolque'),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        TextFormField(
+          textCapitalization: TextCapitalization.words,
+          autocorrect: false,
+          readOnly: true,
+          keyboardType: TextInputType.text,
+          decoration: InputDecorations.authInputDecorations(
+              hintText: '', labelText: 'Número de ejes del remolque'),
+        ),
+      ]);
+    }
+
+    return SingleChildScrollView(
+      child: Form(
+        key: inspeccionForm.formKey,
         child: Column(
           children: [
             DropdownButtonFormField(
@@ -112,99 +210,62 @@ class InspectionScreen extends StatelessWidget {
             const SizedBox(
               height: 10,
             ),
-            ListTile(
-              contentPadding: const EdgeInsets.all(16.0),
-              title: const Text('¿Realizó tanqueo?'),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: RadioListTile<SiNo>(
-                        title: const Text('Yes'),
-                        value: SiNo.si,
-                        groupValue: _optSelected,
-                        onChanged: (SiNo? value) {
-                          _optSelected = value;
-                        },
-                      ),
-                    ),
-                    Expanded(
-                      child: RadioListTile<SiNo>(
-                        title: const Text('No'),
-                        value: SiNo.no,
-                        groupValue: _optSelected,
-                        onChanged: (SiNo? value) {
-                          _optSelected = value;
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.text,
+              validator: (value) {
+                if (value!.isEmpty) return 'Foto del tacometro';
+                return null;
+              },
+              decoration: InputDecorations.authInputDecorations(
+                  hintText: '',
+                  labelText: 'Foto tacometro',
+                  prefixIcon: Icons.speed),
             ),
-            ListTile(
-              contentPadding: const EdgeInsets.all(16.0),
-              title: const Text('¿Tiene remolque?'),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: RadioListTile<SiNo>(
-                        title: const Text('Yes'),
-                        value: SiNo.si,
-                        groupValue: _optSelected,
-                        onChanged: (SiNo? value) {
-                          _optSelected = value;
-                        },
-                      ),
-                    ),
-                    Expanded(
-                      child: RadioListTile<SiNo>(
-                        title: const Text('No'),
-                        value: SiNo.no,
-                        groupValue: _optSelected,
-                        onChanged: (SiNo? value) {
-                          _optSelected = value;
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+            const SizedBox(
+              height: 10,
             ),
-            ListTile(
-              contentPadding: const EdgeInsets.all(16.0),
-              title: const Text('¿Tiene guía transporte?'),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: RadioListTile<SiNo>(
-                        title: const Text('Yes'),
-                        value: SiNo.si,
-                        groupValue: _optSelected,
-                        onChanged: (SiNo? value) {
-                          _optSelected = value;
-                        },
-                      ),
-                    ),
-                    Expanded(
-                      child: RadioListTile<SiNo>(
-                        title: const Text('No'),
-                        value: SiNo.no,
-                        groupValue: _optSelected,
-                        onChanged: (SiNo? value) {
-                          _optSelected = value;
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+            SwitchListTile.adaptive(
+                value: inspeccionForm.realizoTanqueo,
+                title: const Text('¿Realizó tanqueo?'),
+                activeColor: Colors.green,
+                onChanged: (value) =>
+                    inspeccionForm.updateRealizoTanqueo(value)),
+            SwitchListTile.adaptive(
+                value: inspeccionForm.tieneRemolque,
+                title: const Text('¿Tiene remolque?'),
+                activeColor: Colors.green,
+                onChanged: (value) =>
+                    inspeccionForm.updateTieneRemolque(value)),
+            SwitchListTile.adaptive(
+                value: inspeccionForm.tieneGuia,
+                title: const Text('Tiene guía transporte?'),
+                activeColor: Colors.green,
+                onChanged: (value) => inspeccionForm.updateTieneGuia(value)),
+            const SizedBox(
+              height: 10,
             ),
+            if (inspeccionForm.realizoTanqueo)
+              TextFormField(
+                autocorrect: false,
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value!.isEmpty) return 'Cantidad de galones';
+                  return null;
+                },
+                decoration: InputDecorations.authInputDecorations(
+                    hintText: '',
+                    labelText: 'Cantidad de galones tanqueados',
+                    prefixIcon: Icons.speed),
+              ),
+            const SizedBox(
+              height: 10,
+            ),
+            if (inspeccionForm.tieneRemolque) infoRemolque(),
+            const SizedBox(
+              height: 10,
+            ),
+            if (inspeccionForm.tieneGuia) guiaTransporte()
           ],
         ),
       ),
