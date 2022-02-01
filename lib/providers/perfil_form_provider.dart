0b1ae1645571cbb1d154666593db07afa61dfe45
Elver Rodriguez <adelson9602@gmail.com>
2022-01-31 22:09:29 -0500

    se crea metodo para esteblecer el valor del genero seleccionado

--- lib/providers/perfil_form_provider.dart
+++ lib/providers/perfil_form_provider.dart
@@ -5,9 +5,14 @@ import 'package:app_qinspecting/models/models.dart';
 class PerfilFormProvider extends ChangeNotifier {
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
-  UserData userData;
+  UserData userDataLogged;
 
-  PerfilFormProvider(this.userData);
+  PerfilFormProvider(this.userDataLogged);
+
+  updateGenero(String value) {
+    userDataLogged.persGenero = value;
+    notifyListeners();
+  }
 
   bool isValidForm() {
     return formKey.currentState?.validate() ?? false;
