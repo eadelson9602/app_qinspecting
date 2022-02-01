1950aa88e6b38b52a78fd89e1b4feb3bf394f6c9
Elver Rodriguez <adelson9602@gmail.com>
2022-02-01 17:30:09 -0500

    Se cambia de nombre de exportacion de archivos

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,7 +1,7 @@
 export 'package:app_qinspecting/screens/desktop_screen.dart';
 export 'package:app_qinspecting/screens/home_screen.dart';
-export 'package:app_qinspecting/screens/inspeccion_screen.dart';
+export 'package:app_qinspecting/screens/inspeccion_form.dart';
 export 'package:app_qinspecting/screens/login_screen.dart';
 export 'package:app_qinspecting/screens/signature_screen.dart';
 export 'package:app_qinspecting/screens/profile_screen.dart';
-export 'package:app_qinspecting/screens/scaffold_inspeccion_screen.dart';
+export 'package:app_qinspecting/screens/inspeccion_screen.dart';
