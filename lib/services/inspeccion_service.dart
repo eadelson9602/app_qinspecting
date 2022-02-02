924d949bd9fb9de744312ccbe7af7825bf6a2dfc
Elver Rodriguez <adelson9602@gmail.com>
2022-02-01 23:40:52 -0500

    se agrega petición para obtner las ciudades

--- lib/services/inspeccion_service.dart
+++ lib/services/inspeccion_service.dart
@@ -8,11 +8,13 @@ class InspeccionService extends ChangeNotifier {
   bool isLoading = false;
   bool isSaving = false;
   final List<Departamentos> departamentos = [];
+  final List<Ciudades> ciudades = [];
 
   Empresa empresaSelected;
 
   InspeccionService(this.empresaSelected) {
     getDepartamentos();
+    getCiudades();
   }
 
   Future<List<Departamentos>> getDepartamentos() async {
@@ -31,5 +33,24 @@ class InspeccionService extends ChangeNotifier {
     isLoading = false;
     notifyListeners();
     return departamentos;
+  }
+
+  Future<List<Ciudades>> getCiudades() async {
+    isLoading = true;
+    notifyListeners();
+    final baseEmpresa = empresaSelected.nombreBase;
+
+    Response response = await dio
+        .get('https://apis.qinspecting.com/pflutter/list_city/$baseEmpresa');
+    for (var item in response.data) {
+      final tempCiudad = Ciudades.fromMap(item);
+      final index =
+          ciudades.indexWhere((element) => element.value == tempCiudad.value);
+      if (index == -1) ciudades.add(tempCiudad);
+    }
+    print(ciudades[0].label);
+    isLoading = false;
+    notifyListeners();
+    return ciudades;
   }
 }
