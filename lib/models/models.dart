a8a8f950e821ea416cfdb421fe12465d384fed24
Elver Rodriguez <adelson9602@gmail.com>
2022-02-01 23:40:27 -0500

    Se cambia nombre de archivo exportado a departamentos_ciudades.dart

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,3 +1,3 @@
 export 'package:app_qinspecting/models/empresa.dart';
 export 'package:app_qinspecting/models/user_data.dart';
-export 'package:app_qinspecting/models/departamentos.dart';
+export 'package:app_qinspecting/models/departamentos_ciudad.dart';
