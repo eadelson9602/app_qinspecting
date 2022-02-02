5412a18651f19c2521e5f73bbd793d80623b808a
Elver Rodriguez <adelson9602@gmail.com>
2022-02-01 22:34:00 -0500

    Se organiza export

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,3 +1,4 @@
-export 'package:app_qinspecting/providers/ui_provider.dart';
-export 'package:app_qinspecting/providers/login_form_provider.dart';
+export 'package:app_qinspecting/providers/db_provider.dart';
 export 'package:app_qinspecting/providers/inspeccion_provider.dart';
+export 'package:app_qinspecting/providers/login_form_provider.dart';
+export 'package:app_qinspecting/providers/ui_provider.dart';
