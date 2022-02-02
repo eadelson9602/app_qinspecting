510481b7d73a950112f7295498e12070ca709e0f
Elver Rodriguez <adelson9602@gmail.com>
2022-02-01 23:15:29 -0500

    Se importa loadingScreen y se crea class LoadHomeScreen para instanciar service inspección

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:app_qinspecting/screens/loading_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -6,26 +7,35 @@ import 'package:app_qinspecting/screens/screens.dart';
 import 'package:app_qinspecting/services/services.dart';
 import 'package:app_qinspecting/widgets/widgets.dart';
 
+class LoadHomeScreen extends StatelessWidget {
+  const LoadHomeScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final loginService = Provider.of<LoginService>(context, listen: false);
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+            create: (_) => InspeccionService(loginService.selectedEmpresa!)),
+      ],
+      child: const HomeScreen(),
+    );
+  }
+}
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final loginService = Provider.of<LoginService>(context, listen: false);
+    final inspeccionService = Provider.of<InspeccionService>(context);
+    if (inspeccionService.isLoading) return const LoadingScreen();
     return Scaffold(
       appBar: const CustomAppBar().createAppBar(),
       drawer: const CustomDrawer(),
-      body: SafeArea(
+      body: const SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: MultiProvider(
-            providers: [
-              ChangeNotifierProvider(
-                  create: (_) =>
-                      InspeccionService(loginService.selectedEmpresa!)),
-            ],
-            child: const _HomePageBody(),
-          ),
+          padding: EdgeInsets.all(20),
+          child: _HomePageBody(),
         ),
       ),
       bottomNavigationBar: const CustomNavigationBar(),
