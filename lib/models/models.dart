1193b5110885d1c92104899adae02cc20055be33
Elver Rodriguez <adelson9602@gmail.com>
2022-02-04 16:13:35 -0500

    se exporta modelo vehiculos

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,3 +1,4 @@
+export 'package:app_qinspecting/models/departamentos_ciudad.dart';
 export 'package:app_qinspecting/models/empresa.dart';
 export 'package:app_qinspecting/models/user_data.dart';
-export 'package:app_qinspecting/models/departamentos_ciudad.dart';
+export 'package:app_qinspecting/models/vehiculos.dart';
