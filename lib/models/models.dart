b0f5abfb53b7059ecdbbc9fa864c46ea243c6228
Elver Rodriguez <adelson9602@gmail.com>
2022-02-07 15:52:57 -0500

    se cambia nombre de archivo al exportar

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -2,4 +2,4 @@ export 'package:app_qinspecting/models/departamentos_ciudad.dart';
 export 'package:app_qinspecting/models/empresa.dart';
 export 'package:app_qinspecting/models/item_inspeccion.dart';
 export 'package:app_qinspecting/models/user_data.dart';
-export 'package:app_qinspecting/models/vehiculos.dart';
+export 'package:app_qinspecting/models/vehiculo.dart';
