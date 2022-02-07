d2b311fc89c56cccf3785e9dfd26c9b81d6a690e
Elver Rodriguez <adelson9602@gmail.com>
2022-02-07 11:07:26 -0500

    Se agrega propiedades al modelo

--- lib/models/item_inspeccion.dart
+++ lib/models/item_inspeccion.dart
@@ -2,12 +2,15 @@ import 'dart:convert';
 
 class ItemInspeccion {
   ItemInspeccion({
+    required this.placa,
+    required this.tipoVehiculo,
     required this.idCategoria,
     required this.categoria,
     required this.idItem,
     required this.item,
   });
-
+  String placa;
+  String tipoVehiculo;
   int idCategoria;
   String categoria;
   int idItem;
@@ -19,6 +22,8 @@ class ItemInspeccion {
   String toJson() => json.encode(toMap());
 
   factory ItemInspeccion.fromMap(Map<String, dynamic> json) => ItemInspeccion(
+        placa: json["placa"],
+        tipoVehiculo: json["tipo_vehiculo"],
         idCategoria: json["id_categoria"],
         categoria: json["categoria"],
         idItem: json["id_item"],
@@ -26,6 +31,8 @@ class ItemInspeccion {
       );
 
   Map<String, dynamic> toMap() => {
+        "placa": placa,
+        "tipo_vehiculo": tipoVehiculo,
         "id_categoria": idCategoria,
         "categoria": categoria,
         "id_item": idItem,
