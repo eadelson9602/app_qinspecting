14068892d43d09185d3a6f41972eff0de8a69b3c
Elver Rodriguez <adelson9602@gmail.com>
2022-02-07 15:52:15 -0500

    Se cambia nombre archivo y clase

---
+++ lib/models/vehiculo.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+
+class Vehiculo {
+  Vehiculo({
+    required this.idVehiculo,
+    required this.placa,
+    required this.idTipoVehiculo,
+    required this.modelo,
+    required this.marca,
+    required this.color,
+    required this.licenciaTransito,
+  });
+
+  int idVehiculo;
+  String placa;
+  int idTipoVehiculo;
+  int modelo;
+  int marca;
+  String color;
+  int licenciaTransito;
+
+  factory Vehiculo.fromJson(String str) => Vehiculo.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory Vehiculo.fromMap(Map<String, dynamic> json) => Vehiculo(
+        idVehiculo: json["id_vehiculo"],
+        placa: json["placa"],
+        idTipoVehiculo: json["id_tipo_vehiculo"],
+        modelo: json["modelo"],
+        marca: json["marca"],
+        color: json["color"],
+        licenciaTransito: json["licencia_transito"] == null
+            ? null
+            : json["licencia_transito"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "id_vehiculo": idVehiculo,
+        "placa": placa,
+        "id_tipo_vehiculo": idTipoVehiculo,
+        "modelo": modelo,
+        "marca": marca,
+        "color": color,
+        "licencia_transito": licenciaTransito == null ? null : licenciaTransito,
+      };
+}
