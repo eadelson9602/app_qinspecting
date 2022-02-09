deb214c8946b954125b3c01629ace07c21351760
Elver Rodriguez <adelson9602@gmail.com>
2022-02-08 22:29:42 -0500

    Se crea card para ver la foto del tacometro

---
+++ lib/widgets/board_image.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class BoardImage extends StatelessWidget {
+  const BoardImage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: _buildBoxDecoration(),
+      height: 250,
+      width: double.infinity,
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        child: FadeInImage(
+          image: NetworkImage('https://via.placeholder.com/400x300/green'),
+          placeholder: AssetImage('assets/images/loading-2.gif'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.all(Radius.circular(45)),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.05),
+                blurRadius: 10,
+                offset: Offset(0, 5))
+          ]);
+}
