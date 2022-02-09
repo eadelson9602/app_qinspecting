7f0b787ea877cd901aaeb4eff1cabccc48b1f248
Elver Rodriguez <adelson9602@gmail.com>
2022-02-08 21:48:01 -0500

    se exporta imagen

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -2,3 +2,4 @@ export 'package:app_qinspecting/widgets/auth_background.dart';
 export 'package:app_qinspecting/widgets/custom_app_bar.dart';
 export 'package:app_qinspecting/widgets/custom_drawer.dart';
 export 'package:app_qinspecting/widgets/custom_navigatorbar.dart';
+export 'package:app_qinspecting/widgets/board_image.dart';
