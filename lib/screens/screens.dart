0e0ed53dd34a8dc555344da6ad08a753a639d19c
Elver Rodriguez <adelson9602@gmail.com>
2022-02-09 23:28:20 -0500

    Se exporta screen send_pending_inspection

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -5,3 +5,4 @@ export 'package:app_qinspecting/screens/login_screen.dart';
 export 'package:app_qinspecting/screens/signature_screen.dart';
 export 'package:app_qinspecting/screens/profile_screen.dart';
 export 'package:app_qinspecting/screens/inspeccion_screen.dart';
+export 'package:app_qinspecting/screens/send_pending_inspection_screen.dart';
