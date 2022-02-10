aaf25abe79b48e52dc2128ee642ed7c4447f2a9e
Elver Rodriguez <adelson9602@gmail.com>
2022-02-10 16:35:10 -0500

    Se exporta widget ItemInspeccion

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -4,3 +4,4 @@ export 'package:app_qinspecting/widgets/custom_drawer.dart';
 export 'package:app_qinspecting/widgets/custom_navigatorbar.dart';
 export 'package:app_qinspecting/widgets/board_image.dart';
 export 'package:app_qinspecting/widgets/signature.dart';
+export 'package:app_qinspecting/models/item_inspeccion.dart';
