cc848d0724bcf2c43bf557c88bb9ae061bc3eea0
Elver Rodriguez <adelson9602@gmail.com>
2022-02-10 00:43:16 -0500

    Se crea vista para mostrar las inspecciones pendientes por enviar al servidor

--- lib/screens/send_pending_inspection_screen.dart
+++ lib/screens/send_pending_inspection_screen.dart
@@ -11,7 +11,31 @@ class SendPendingInspectionScree extends StatelessWidget {
       appBar: const CustomAppBar().createAppBar(),
       drawer: const CustomDrawer(),
       body: Container(
-        child: Text('Inspección Form'),
+        height: double.infinity,
+        padding: EdgeInsets.only(top: 5, left: 10, right: 10, bottom: 50),
+        child: ListView.builder(
+            itemCount: 15,
+            itemBuilder: (_, int i) => ListTile(
+                  iconColor: Colors.green,
+                  shape: Border.all(
+                      style: BorderStyle.solid,
+                      color: Colors.green,
+                      width: 0.2),
+                  leading: Icon(
+                    Icons.find_in_page,
+                  ),
+                  title: Text('Hola'),
+                  subtitle: Text('10/02/2022'),
+                  trailing: Icon(
+                    Icons.upload,
+                  ),
+                )),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        mini: true,
+        onPressed: () {},
+        child: Icon(Icons.upload_rounded),
       ),
     );
   }
