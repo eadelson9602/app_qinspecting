7616acbd34b416da66be8502cf1cf92cd6c749a7
Elver Rodriguez <adelson9602@gmail.com>
2022-02-09 22:40:03 -0500

    Se exporta widget signature

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -3,3 +3,4 @@ export 'package:app_qinspecting/widgets/custom_app_bar.dart';
 export 'package:app_qinspecting/widgets/custom_drawer.dart';
 export 'package:app_qinspecting/widgets/custom_navigatorbar.dart';
 export 'package:app_qinspecting/widgets/board_image.dart';
+export 'package:app_qinspecting/widgets/signature.dart';
