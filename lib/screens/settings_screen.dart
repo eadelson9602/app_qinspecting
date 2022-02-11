cc1cac4536f9be713a025ce715ec9d5074f0566e
Elver Rodriguez <adelson9602@gmail.com>
2022-02-10 22:45:17 -0500

    Se crea screen para configuraciones de la app

---
+++ lib/screens/settings_screen.dart
@@ -0,0 +1,18 @@
+import 'package:app_qinspecting/providers/providers.dart';
+import 'package:flutter/material.dart';
+
+// import 'package:app_qinspecting/widgets/widgets.dart';
+import 'package:provider/provider.dart';
+
+class SettingScreen extends StatelessWidget {
+  const SettingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text('Pagina settings'),
+      ),
+    );
+  }
+}
