a4735211214e7c476035ec797cfe3198708d3ce2
Elver Rodriguez <adelson9602@gmail.com>
2022-02-10 23:12:30 -0500

    Se quita scaffold

--- lib/widgets/items_inspeccion.dart
+++ lib/widgets/items_inspeccion.dart
@@ -9,28 +9,25 @@ class ItemsInspeccionar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final inspeccionProvider = Provider.of<InspeccionProvider>(context);
-    return Scaffold(
-      body: Container(
-          child: Stepper(
-        margin: EdgeInsets.only(left: 55),
-        elevation: 10,
-        currentStep: inspeccionProvider.stepStepper,
-        onStepCancel: () {
-          if (inspeccionProvider.stepStepper > 0) {
-            inspeccionProvider.updateStep(inspeccionProvider.stepStepper -= 1);
-          }
-        },
-        onStepContinue: () {
-          if (inspeccionProvider.stepStepper <
-              inspeccionProvider.steps.length) {
-            inspeccionProvider.updateStep(inspeccionProvider.stepStepper += 1);
-          }
-        },
-        onStepTapped: (int index) {
-          inspeccionProvider.updateStep(index);
-        },
-        steps: inspeccionProvider.steps,
-      )),
-    );
+    return Container(
+        child: Stepper(
+      margin: EdgeInsets.only(left: 55),
+      elevation: 10,
+      currentStep: inspeccionProvider.stepStepper,
+      onStepCancel: () {
+        if (inspeccionProvider.stepStepper > 0) {
+          inspeccionProvider.updateStep(inspeccionProvider.stepStepper -= 1);
+        }
+      },
+      onStepContinue: () {
+        if (inspeccionProvider.stepStepper < inspeccionProvider.steps.length) {
+          inspeccionProvider.updateStep(inspeccionProvider.stepStepper += 1);
+        }
+      },
+      onStepTapped: (int index) {
+        inspeccionProvider.updateStep(index);
+      },
+      steps: inspeccionProvider.steps,
+    ));
   }
 }
