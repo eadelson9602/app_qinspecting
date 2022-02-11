c2bc8ddab39e65f159d3843ccc7e151af519e2fb
Elver Rodriguez <adelson9602@gmail.com>
2022-02-10 22:44:12 -0500

    Se crea widget stepper para mostrar los items de inspección

--- lib/widgets/items_inspeccion.dart
+++ lib/widgets/items_inspeccion.dart
@@ -3,41 +3,34 @@ import 'package:provider/provider.dart';
 
 import 'package:app_qinspecting/providers/providers.dart';
 
-class ItemInspeccion extends StatelessWidget {
-  const ItemInspeccion({Key? key}) : super(key: key);
+class ItemsInspeccionar extends StatelessWidget {
+  const ItemsInspeccionar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final inspeccionProvider = Provider.of<InspeccionProvider>(context);
-    // inspeccionProvider.itemsInspeccion.length;
-    return Stepper(
-      currentStep: inspeccionProvider.stepStepper,
-      onStepCancel: () {
-        if (inspeccionProvider.stepStepper > 0) {
-          inspeccionProvider.stepStepper -= 1;
-        }
-      },
-      onStepContinue: () {
-        if (inspeccionProvider.stepStepper <= 0) {
-          inspeccionProvider.stepStepper += 1;
-        }
-      },
-      onStepTapped: (int index) {
-        inspeccionProvider.stepStepper = index;
-      },
-      steps: [
-        Step(
-          title: const Text('Step 1 title'),
-          content: Container(
-              alignment: Alignment.centerLeft,
-              child: const Text('Content for Step 1')),
-        ),
-        const Step(
-          title: Text('Step 2 title'),
-          content: Text('Content for Step 2'),
-        ),
-      ],
+    return Scaffold(
+      body: Container(
+          child: Stepper(
+        margin: EdgeInsets.only(left: 55),
+        elevation: 10,
+        currentStep: inspeccionProvider.stepStepper,
+        onStepCancel: () {
+          if (inspeccionProvider.stepStepper > 0) {
+            inspeccionProvider.updateStep(inspeccionProvider.stepStepper -= 1);
+          }
+        },
+        onStepContinue: () {
+          if (inspeccionProvider.stepStepper <
+              inspeccionProvider.steps.length) {
+            inspeccionProvider.updateStep(inspeccionProvider.stepStepper += 1);
+          }
+        },
+        onStepTapped: (int index) {
+          inspeccionProvider.updateStep(index);
+        },
+        steps: inspeccionProvider.steps,
+      )),
     );
-    ;
   }
 }
