2588a8c6e1d612550bef7017f7ce933d9622c025
Elver Rodriguez <adelson9602@gmail.com>
2022-02-11 23:10:53 -0500

    Se agrega form para mostrar los items de inspección, campo de observaciones y la foto

--- lib/widgets/items_inspeccion.dart
+++ lib/widgets/items_inspeccion.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 
 import 'package:app_qinspecting/providers/providers.dart';
+import 'package:app_qinspecting/widgets/widgets.dart';
 
 class ItemsInspeccionar extends StatelessWidget {
   const ItemsInspeccionar({Key? key}) : super(key: key);
@@ -9,10 +11,123 @@ class ItemsInspeccionar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final inspeccionProvider = Provider.of<InspeccionProvider>(context);
-    return Container(
-        child: Stepper(
+    final itemsInspeccionar = inspeccionProvider.itemsInspeccion;
+    List<Step> _renderSteps() {
+      List<Step> stepsInspeccion = [];
+      for (int i = 0; i < itemsInspeccionar.length; i++) {
+        stepsInspeccion.add(Step(
+            isActive: inspeccionProvider.stepStepper >= i ? true : false,
+            title: Text(itemsInspeccionar[i].categoria),
+            content: Column(
+              children: [
+                for (var item in itemsInspeccionar[i].items)
+                  Column(
+                    children: [
+                      Container(
+                          width: double.infinity,
+                          child: Text(
+                            item.item,
+                            textAlign: TextAlign.start,
+                            maxLines: 2,
+                          )),
+                      Row(
+                        children: [
+                          Radio(
+                            activeColor: Colors.green,
+                            groupValue: '',
+                            value: 'value',
+                            onChanged: (value) {
+                              print(value);
+                            },
+                          ),
+                          Text(
+                            'Cumple',
+                            style: TextStyle(color: Colors.green),
+                          ),
+                          Radio(
+                            activeColor: Colors.red,
+                            groupValue: '',
+                            value: 'value',
+                            onChanged: (value) {
+                              print(value);
+                            },
+                          ),
+                          Text(
+                            'No cumple',
+                            style: TextStyle(color: Colors.red),
+                          ),
+                          Radio(
+                            activeColor: Colors.orange,
+                            groupValue: '',
+                            value: 'value',
+                            onChanged: (value) {
+                              print(value);
+                            },
+                          ),
+                          Text(
+                            'N/A',
+                            style: TextStyle(color: Colors.orange),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 11),
+                      Container(
+                        width: double.infinity,
+                        padding: EdgeInsets.only(right: 20, left: 15),
+                        child: TextField(
+                          maxLines: 8,
+                          decoration: InputDecoration(
+                              hintText: "Observaciones",
+                              filled: true,
+                              contentPadding: EdgeInsets.all(10.0),
+                              floatingLabelBehavior:
+                                  FloatingLabelBehavior.always),
+                        ),
+                      ),
+                      SizedBox(height: 11),
+                      Container(
+                        width: 270,
+                        child: Stack(
+                          children: [
+                            BoardImage(
+                              url: inspeccionProvider.pathFile,
+                            ),
+                            Positioned(
+                                right: 15,
+                                bottom: 10,
+                                child: IconButton(
+                                  onPressed: () async {
+                                    final _picker = ImagePicker();
+                                    final XFile? photo = await _picker
+                                        .pickImage(source: ImageSource.camera);
+
+                                    if (photo == null) {
+                                      return;
+                                    }
+                                    // Se asigna la imagen en un provider para esta sección
+                                    inspeccionProvider
+                                        .updateSelectedImage(photo.path);
+                                  },
+                                  icon: Icon(
+                                    Icons.camera_alt,
+                                    color: Colors.white,
+                                    size: 45,
+                                  ),
+                                ))
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 11),
+                    ],
+                  ),
+              ],
+            )));
+      }
+      return stepsInspeccion;
+    }
+
+    return Stepper(
       margin: EdgeInsets.only(left: 55),
-      elevation: 10,
       currentStep: inspeccionProvider.stepStepper,
       onStepCancel: () {
         if (inspeccionProvider.stepStepper > 0) {
@@ -20,14 +135,14 @@ class ItemsInspeccionar extends StatelessWidget {
         }
       },
       onStepContinue: () {
-        if (inspeccionProvider.stepStepper < inspeccionProvider.steps.length) {
+        if (inspeccionProvider.stepStepper < itemsInspeccionar.length) {
           inspeccionProvider.updateStep(inspeccionProvider.stepStepper += 1);
         }
       },
       onStepTapped: (int index) {
         inspeccionProvider.updateStep(index);
       },
-      steps: inspeccionProvider.steps,
-    ));
+      steps: _renderSteps(),
+    );
   }
 }
