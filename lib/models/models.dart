b557c9fceccf2f136284fc23633005739f5bb30e
Elver Rodriguez <adelson9602@gmail.com>
2022-02-12 14:25:27 -0500

    Se exporta modelo inspeccion

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -3,3 +3,4 @@ export 'package:app_qinspecting/models/empresa.dart';
 export 'package:app_qinspecting/models/item_inspeccion.dart';
 export 'package:app_qinspecting/models/user_data.dart';
 export 'package:app_qinspecting/models/vehiculo.dart';
+export 'package:app_qinspecting/models/inspeccion.dart';
