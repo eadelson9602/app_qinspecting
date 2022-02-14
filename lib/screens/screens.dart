1348a620d8b52eda17eed4c2484356f241db25c7
Elver Rodriguez <adelson9602@gmail.com>
2022-02-13 22:17:53 -0500

    Se exporta loading screen

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,9 +1,10 @@
 export 'package:app_qinspecting/screens/desktop_screen.dart';
 export 'package:app_qinspecting/screens/home_screen.dart';
 export 'package:app_qinspecting/screens/inspeccion_form.dart';
-export 'package:app_qinspecting/screens/login_screen.dart';
-export 'package:app_qinspecting/screens/signature_screen.dart';
-export 'package:app_qinspecting/screens/profile_screen.dart';
 export 'package:app_qinspecting/screens/inspeccion_screen.dart';
+export 'package:app_qinspecting/screens/loading_screen.dart';
+export 'package:app_qinspecting/screens/login_screen.dart';
+export 'package:app_qinspecting/screens/profile_screen.dart';
 export 'package:app_qinspecting/screens/send_pending_inspection_screen.dart';
 export 'package:app_qinspecting/screens/settings_screen.dart';
+export 'package:app_qinspecting/screens/signature_screen.dart';
