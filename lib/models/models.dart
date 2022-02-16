0b4a93901464b93a7cc65b28eb78822212a447e9
Elver Rodriguez <adelson9602@gmail.com>
2022-02-15 23:47:52 -0500

    Se exporta modelo remolque

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -4,3 +4,4 @@ export 'package:app_qinspecting/models/item_inspeccion.dart';
 export 'package:app_qinspecting/models/user_data.dart';
 export 'package:app_qinspecting/models/vehiculo.dart';
 export 'package:app_qinspecting/models/inspeccion.dart';
+export 'package:app_qinspecting/models/remolque.dart';
