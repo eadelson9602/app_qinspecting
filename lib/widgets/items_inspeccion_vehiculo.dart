e6cee5bc6786429d5b53a15a5c2c71e7484727db
Elver Rodriguez <adelson9602@gmail.com>
2022-02-17 17:11:43 -0500

    se cambia  nombre widget para estilos del btn

--- lib/widgets/items_inspeccion_vehiculo.dart
+++ lib/widgets/items_inspeccion_vehiculo.dart
@@ -32,7 +32,7 @@ class _ItemsInspeccionarStateVehiculo extends State<ItemsInspeccionarVehiculo> {
                     elevation: 3,
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(100)),
-                    child: TextButtonLogin(),
+                    child: TextButtonAllok(),
                     onPressed: () {
                       setState(() {
                         for (var item in itemsInspeccionar[i].items) {
@@ -183,40 +183,6 @@ class _ItemsInspeccionarStateVehiculo extends State<ItemsInspeccionarVehiculo> {
         inspeccionProvider.updateStep(index);
       },
       steps: _renderSteps(),
-    );
-  }
-}
-
-class TextButtonLogin extends StatelessWidget {
-  const TextButtonLogin({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 150,
-      padding: const EdgeInsets.symmetric(vertical: 10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            Icons.check,
-            color: Colors.white,
-          ),
-          SizedBox(
-            width: 10,
-          ),
-          Text(
-            'Todo ok',
-            style: const TextStyle(color: Colors.white, fontSize: 18),
-          )
-        ],
-      ),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(100),
-          gradient: const LinearGradient(colors: [
-            Color.fromRGBO(31, 133, 53, 1),
-            Color.fromRGBO(103, 210, 0, 1)
-          ])),
     );
   }
 }
