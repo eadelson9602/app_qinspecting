0e9c648d78ebe92893b15f72b6a8a3487098a3c2
Elver Rodriguez <adelson9602@gmail.com>
2022-02-16 21:04:19 -0500

    Se cambia nombre archivo

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -4,4 +4,4 @@ export 'package:app_qinspecting/widgets/custom_drawer.dart';
 export 'package:app_qinspecting/widgets/custom_navigatorbar.dart';
 export 'package:app_qinspecting/widgets/board_image.dart';
 export 'package:app_qinspecting/widgets/signature.dart';
-export 'package:app_qinspecting/widgets/items_inspeccion.dart';
+export 'package:app_qinspecting/widgets/items_inspeccion_vehiculo.dart';
