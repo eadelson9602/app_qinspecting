40e2b9f9792d69e634fc12c3caad3037dab05f29
Elver Rodriguez <adelson9602@gmail.com>
2022-02-16 20:44:14 -0500

    Se cambia tipo retorno

--- lib/providers/db_provider.dart
+++ lib/providers/db_provider.dart
@@ -187,11 +187,11 @@ class DBProvider {
     return res!.isNotEmpty ? Vehiculo.fromMap(res.first) : null;
   }
 
-  Future<Vehiculo?> getRemolqueByPlate(String placa) async {
+  Future<Remolque?> getRemolqueByPlate(String placa) async {
     final db = await database;
     final res =
         await db?.query('Remolques', where: 'placa = ?', whereArgs: [placa]);
-    return res!.isNotEmpty ? Vehiculo.fromMap(res.first) : null;
+    return res!.isNotEmpty ? Remolque.fromMap(res.first) : null;
   }
 
   Future<List<Vehiculo>?> getAllVehiculos() async {
