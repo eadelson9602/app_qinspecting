f7afbfa7b844376c7e31a1b4b19dc0ad5f2abdea
Elver Rodriguez <adelson9602@gmail.com>
2022-02-16 22:45:55 -0500

    Se agrega widget para mostar los items de inspecciónd el remolque

---
+++ lib/widgets/items_inspeccion_remolque.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
+
+import 'package:app_qinspecting/providers/providers.dart';
+import 'package:app_qinspecting/screens/screens.dart';
+import 'package:app_qinspecting/widgets/widgets.dart';
+
+class ItemsInspeccionarRemolque extends StatefulWidget {
+  ItemsInspeccionarRemolque({Key? key}) : super(key: key);
+
+  @override
+  State<ItemsInspeccionarRemolque> createState() =>
+      _ItemsInspeccionarStateRemolque();
+}
+
+class _ItemsInspeccionarStateRemolque extends State<ItemsInspeccionarRemolque> {
+  @override
+  Widget build(BuildContext context) {
+    final inspeccionProvider = Provider.of<InspeccionProvider>(context);
+    final itemsInspeccionar = inspeccionProvider.itemsInspeccionRemolque;
+    List<Step> _renderSteps() {
+      List<Step> stepsInspeccion = [];
+      for (int i = 0; i < itemsInspeccionar.length; i++) {
+        stepsInspeccion.add(Step(
+            isActive: inspeccionProvider.stepStepper >= i ? true : false,
+            title: Text(itemsInspeccionar[i].categoria),
+            content: Column(
+              children: [
+                for (var item in itemsInspeccionar[i].items)
+                  Column(
+                    children: [
+                      Container(
+                          width: double.infinity,
+                          child: Text(
+                            item.item,
+                            textAlign: TextAlign.start,
+                            maxLines: 2,
+                          )),
+                      Row(
+                        children: [
+                          Radio(
+                            activeColor: Colors.green,
+                            groupValue: item.respuesta,
+                            value: 'B',
+                            onChanged: (value) {
+                              setState(() {
+                                item.respuesta = value.toString();
+                              });
+                            },
+                          ),
+                          Text(
+                            'Cumple',
+                            style: TextStyle(color: Colors.green),
+                          ),
+                          Radio(
+                            activeColor: Colors.red,
+                            groupValue: item.respuesta,
+                            value: 'M',
+                            onChanged: (value) {
+                              setState(() {
+                                item.respuesta = value.toString();
+                              });
+                            },
+                          ),
+                          Text(
+                            'No cumple',
+                            style: TextStyle(color: Colors.red),
+                          ),
+                          Radio(
+                            activeColor: Colors.orange,
+                            groupValue: item.respuesta,
+                            value: 'N/A',
+                            onChanged: (value) {
+                              setState(() {
+                                item.respuesta = value.toString();
+                              });
+                            },
+                          ),
+                          Text(
+                            'N/A',
+                            style: TextStyle(color: Colors.orange),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 11),
+                      Container(
+                        width: double.infinity,
+                        padding: EdgeInsets.only(right: 20, left: 15),
+                        child: TextField(
+                          maxLines: 8,
+                          decoration: InputDecoration(
+                              hintText: "Observaciones",
+                              filled: true,
+                              contentPadding: EdgeInsets.all(10.0),
+                              floatingLabelBehavior:
+                                  FloatingLabelBehavior.always),
+                          onChanged: (value) {
+                            item.observaciones = value;
+                          },
+                        ),
+                      ),
+                      SizedBox(height: 11),
+                      if (item.respuesta == 'M')
+                        Container(
+                          width: 270,
+                          child: Stack(
+                            children: [
+                              BoardImage(
+                                url: item.adjunto,
+                              ),
+                              Positioned(
+                                  right: 15,
+                                  bottom: 10,
+                                  child: IconButton(
+                                    onPressed: () async {
+                                      final _picker = ImagePicker();
+                                      final XFile? photo =
+                                          await _picker.pickImage(
+                                              source: ImageSource.camera);
+
+                                      if (photo == null) {
+                                        return;
+                                      }
+                                      setState(() {
+                                        // Se asigna la imagen en un provider para esta sección
+                                        item.adjunto = photo.path;
+                                      });
+                                    },
+                                    icon: Icon(
+                                      Icons.camera_alt,
+                                      color: Colors.white,
+                                      size: 45,
+                                    ),
+                                  ))
+                            ],
+                          ),
+                        ),
+                      SizedBox(height: 11),
+                    ],
+                  ),
+              ],
+            )));
+      }
+      return stepsInspeccion;
+    }
+
+    if (itemsInspeccionar.isEmpty) return const LoadingScreen();
+    return Stepper(
+      margin: EdgeInsets.only(left: 55),
+      currentStep: inspeccionProvider.stepStepper,
+      onStepCancel: () {
+        if (inspeccionProvider.stepStepper > 0) {
+          inspeccionProvider.updateStep(inspeccionProvider.stepStepper -= 1);
+        }
+      },
+      onStepContinue: () {
+        if (inspeccionProvider.stepStepper < itemsInspeccionar.length) {
+          inspeccionProvider.updateStep(inspeccionProvider.stepStepper += 1);
+        }
+      },
+      onStepTapped: (int index) {
+        inspeccionProvider.updateStep(index);
+      },
+      steps: _renderSteps(),
+    );
+  }
+}
