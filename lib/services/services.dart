9553d817072768de147d3c91bcc1dabb98fe9b8f
Elver Rodriguez <adelson9602@gmail.com>
2022-03-13 21:56:46 -0500

    Se exporta servicio firma

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,2 +1,3 @@
 export 'package:app_qinspecting/services/login_service.dart';
 export 'package:app_qinspecting/services/inspeccion_service.dart';
+export 'package:app_qinspecting/services/firma_service.dart';
