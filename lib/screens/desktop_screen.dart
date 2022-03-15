e3f668a66b4e19ed2015c1da76a1af73cb20c1f8
Elver Rodriguez <adelson9602@gmail.com>
2022-03-15 12:34:03 -0500

    Se mejora card para mostrar inspecciones del server

--- lib/screens/desktop_screen.dart
+++ lib/screens/desktop_screen.dart
@@ -1,45 +1,34 @@
-import 'package:app_qinspecting/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'package:app_qinspecting/providers/inspeccion_provider.dart';
 import 'package:app_qinspecting/services/services.dart';
+import 'package:app_qinspecting/widgets/widgets.dart';
 
 class DesktopScreen extends StatelessWidget {
   const DesktopScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final inspeccionProvider = Provider.of<InspeccionProvider>(context);
+    final loginService = Provider.of<LoginService>(context);
     final inspeccionService = Provider.of<InspeccionService>(context);
 
-    final allInspecciones = inspeccionProvider.allInspecciones;
-    inspeccionProvider.cargarTodosInspecciones();
-
     return Container(
-      height: double.infinity,
-      padding: EdgeInsets.only(top: 5, left: 10, right: 10, bottom: 50),
-      child: ListView.builder(
-          itemCount: allInspecciones.length,
-          itemBuilder: (_, int i) {
-            if (inspeccionService.isSaving)
-              return Container(
-                padding: EdgeInsets.all(20),
-                child: Column(children: [
-                  Image(
-                    image: AssetImage('assets/images/loading_3.gif'),
-                    // fit: BoxFit.cover,
-                    height: 50,
-                  ),
-                  SizedBox(
-                    width: 30,
-                  ),
-                  LinearProgressIndicator(),
-                ]),
-              );
-            return CardInspeccionDesktop(
-                resumenPreoperacional: allInspecciones[i]);
-          }),
-    );
+        height: double.infinity,
+        child: FutureBuilder(
+            future: inspeccionService
+                .getLatesInspections(loginService.selectedEmpresa),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else {
+                List data = snapshot.data as List;
+                return ListView.builder(
+                    itemCount: data.length,
+                    itemBuilder: (_, int i) {
+                      return CardInspeccionDesktop(
+                          resumenPreoperacional: data[i]);
+                    });
+              }
+            }));
   }
 }
