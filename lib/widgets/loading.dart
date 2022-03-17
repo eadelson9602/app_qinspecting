a179ae8ec5dc6d19dd759dbe8cbc03d63e21fc2f
Elver Rodriguez <adelson9602@gmail.com>
2022-03-16 21:25:10 -0500

    Se da estilos al loading

--- lib/widgets/loading.dart
+++ lib/widgets/loading.dart
@@ -5,8 +5,28 @@ class Loading extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-    );
+    return Scaffold(
+        body: Container(
+      color: const Color(0xFFFFFFFF),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Center(
+            child: Image(
+              image: AssetImage('assets/images/loading_4.gif'),
+              width: 180,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Guardando...',
+            style: TextStyle(
+                fontWeight: FontWeight.bold, fontSize: 18, color: Colors.black),
+          )
+        ],
+      ),
+    ));
   }
 }
