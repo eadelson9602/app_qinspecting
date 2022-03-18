e2b1965594179fc83539f7a6b55a8afd3d0e81c3
Elver Rodriguez <adelson9602@gmail.com>
2022-03-17 22:34:08 -0500

    Se agrega screen para obtener los datos necesarios de la app

---
+++ lib/screens/get_data_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:app_qinspecting/services/services.dart';
+
+class GetDataScreen extends StatelessWidget {
+  const GetDataScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final inspeccionService = Provider.of<InspeccionService>(context);
+    final loginService = Provider.of<LoginService>(context, listen: false);
+    return Scaffold(
+        body: FutureBuilder(
+            future: inspeccionService.getData(loginService.selectedEmpresa),
+            builder: (ctx, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else {
+                return Center(
+                  child: Text(
+                    'Cargado al 100',
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                );
+              }
+            }));
+  }
+}
