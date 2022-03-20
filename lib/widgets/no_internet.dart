b7194a6cc1170d5d2e2381aed4d61eafa23de4ad
Elver Rodriguez <adelson9602@gmail.com>
2022-03-19 19:03:51 -0500

    Se crea y se exporta widget no internet

---
+++ lib/widgets/no_internet.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class NoInternet extends StatelessWidget {
+  const NoInternet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Image(
+            image: AssetImage('assets/images/boot.gif'),
+            // fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Oops!!',
+            style: TextStyle(fontSize: 34, color: Colors.black38),
+            textAlign: TextAlign.center,
+          ),
+          Text(
+            'Debe conectarse a internet para ver las inspecciones enviadas...',
+            style: TextStyle(fontSize: 24, color: Colors.black38),
+            textAlign: TextAlign.center,
+          )
+        ],
+      ),
+    );
+  }
+}
