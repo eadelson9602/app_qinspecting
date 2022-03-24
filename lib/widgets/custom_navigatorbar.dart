f40e9922aef39c5cddc3e87099b586e1e41d421a
Elver Rodriguez <adelson9602@gmail.com>
2022-03-23 20:55:47 -0500

    Se agrega validación de firma

--- lib/widgets/custom_navigatorbar.dart
+++ lib/widgets/custom_navigatorbar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:app_qinspecting/providers/providers.dart';
+import 'package:app_qinspecting/services/services.dart';
 
 class CustomNavigationBar extends StatelessWidget {
   const CustomNavigationBar({Key? key}) : super(key: key);
@@ -9,6 +10,7 @@ class CustomNavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final uiProvider = Provider.of<UiProvider>(context);
+    final loginService = Provider.of<LoginService>(context);
     final currentIdex = uiProvider.selectedMenuOpt;
 
     return BottomNavigationBar(
@@ -25,7 +27,13 @@ class CustomNavigationBar extends StatelessWidget {
       currentIndex: currentIdex,
       selectedItemColor: Colors.green,
       onTap: (int i) {
-        uiProvider.selectedMenuOpt = i;
+        if (i == 1 && loginService.userDataLogged.firmaId != 0) {
+          uiProvider.selectedMenuOpt = i;
+        } else if (i == 1 && loginService.userDataLogged.firmaId == 0) {
+          Navigator.popAndPushNamed(context, 'signature');
+        } else {
+          uiProvider.selectedMenuOpt = i;
+        }
       },
     );
   }
