c38d947841b248f3bdb8fb8783cc0e624f58117b
Elver Rodriguez <adelson9602@gmail.com>
2022-04-04 20:10:29 -0500

    se agrega swiper

--- lib/screens/desktop_screen.dart
+++ lib/screens/desktop_screen.dart
@@ -1,7 +1,8 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:date_range_form_field/date_range_form_field.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
 import 'package:app_qinspecting/services/services.dart';
 import 'package:app_qinspecting/widgets/widgets.dart';
@@ -14,7 +15,6 @@ class DesktopScreen extends StatefulWidget {
 }
 
 class _DesktopScreenState extends State<DesktopScreen> {
-  GlobalKey<FormState> myFormKey = new GlobalKey();
   @override
   Widget build(BuildContext context) {
     final loginService = Provider.of<LoginService>(context);
@@ -28,10 +28,13 @@ class _DesktopScreenState extends State<DesktopScreen> {
           return Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              DateRange(inspeccionService, sizeScreen),
               SizedBox(
-                height: 15,
+                height: 10,
               ),
+              // DateRange(loginService, inspeccionService, sizeScreen),
+              // SizedBox(
+              //   height: 15,
+              // ),
               Text(
                 'Inspecciones realizadas',
                 style: TextStyle(fontSize: 18),
@@ -45,33 +48,20 @@ class _DesktopScreenState extends State<DesktopScreen> {
                     } else {
                       List data = snapshot.data as List;
                       return Container(
-                        height: 370,
-                        child: ListView.builder(
-                            shrinkWrap: true,
-                            scrollDirection: Axis.horizontal,
-                            itemCount: data.length,
-                            itemBuilder: (_, int i) => CardInspeccionDesktop(
-                                resumenPreoperacional: data[i])),
+                        height: sizeScreen.height * 0.5,
+                        child: Swiper(
+                          layout: SwiperLayout.STACK,
+                          itemHeight: sizeScreen.height * 0.9,
+                          itemWidth: sizeScreen.height * 0.5,
+                          itemBuilder: (BuildContext context, int i) {
+                            return CardInspeccionDesktop(
+                                resumenPreoperacional: data[i]);
+                          },
+                          itemCount: data.length,
+                        ),
                       );
                     }
                   }),
-              // Text(
-              //   'Demo Headline 2',
-              //   style: TextStyle(fontSize: 18),
-              // ),
-              // Expanded(
-              //   child: ListView.builder(
-              //     shrinkWrap: true,
-              //     itemBuilder: (ctx, int) {
-              //       return Card(
-              //         child: ListTile(
-              //             title: Text('Motivation $int'),
-              //             subtitle:
-              //                 Text('this is a description of the motivation')),
-              //       );
-              //     },
-              //   ),
-              // ),
             ],
           );
         } else {
@@ -81,46 +71,58 @@ class _DesktopScreenState extends State<DesktopScreen> {
     );
   }
 
-  Form DateRange(InspeccionService inspeccionService, sizeScreen) {
+  Form DateRange(LoginService loginService, InspeccionService inspeccionService,
+      sizeScreen) {
     return Form(
-      key: myFormKey,
       child: Column(
         children: [
-          DateRangeField(
-              width: sizeScreen.width * 1,
-              enabled: true,
-              margin: EdgeInsets.only(bottom: 0, top: 20),
-              decoration: InputDecoration(
-                labelText: 'Rango de fecha',
-                prefixIcon: Icon(Icons.date_range),
-                hintText: '--/--/-- --/--/--',
-                border: OutlineInputBorder(),
-              ),
-              validator: (value) {
-                if (value == null) {
-                  return 'Seleccione un rango de fecha';
-                }
-                return null;
-              },
-              onChanged: (DateTimeRange? value) {
-                inspeccionService.updateDate(value!);
-              },
-              onSaved: (DateTimeRange? value) {
-                inspeccionService.updateDate(value!);
-              }),
           ElevatedButton(
             child: Container(
-              width: 70,
+              width: 200,
               child: Row(
-                children: [Icon(Icons.search), Text('Buscar')],
+                children: [
+                  Icon(Icons.calendar_today),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text('Filtrar por rango de fecha')
+                ],
               ),
             ),
-            onPressed: () {
-              bool isValidForm() {
-                return myFormKey.currentState?.validate() ?? false;
-              }
+            onPressed: () async {
+              await Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) {
+                    return Scaffold(
+                      appBar: AppBar(),
+                      body: SfDateRangePicker(
+                        enablePastDates: true,
+                        view: DateRangePickerView.month,
+                        selectionMode: DateRangePickerSelectionMode.range,
+                        monthViewSettings:
+                            DateRangePickerMonthViewSettings(firstDayOfWeek: 1),
+                        showActionButtons: true,
+                        cancelText: 'Cancelar',
+                        confirmText: 'Buscar',
+                        onCancel: () => Navigator.pop(context),
+                        onSubmit: (val) async {
+                          PickerDateRange date = val as PickerDateRange;
+                          final initialDate =
+                              date.startDate.toString().split(' ')[0];
+                          final endDate = date.endDate.toString().split(' ')[0];
 
-              if (!isValidForm()) return;
+                          await inspeccionService.getRangeInspections(
+                              loginService.selectedEmpresa,
+                              initialDate,
+                              endDate);
+
+                          Navigator.pop(context);
+                        },
+                      ),
+                    );
+                  },
+                ),
+              );
             },
           ),
         ],
