a3b372b34446788a1c0343fdbb68626b029636b7
Elver Rodriguez <adelson9602@gmail.com>
2022-04-21 18:36:09 -0500

    Se esta configurando tabla para pdf

--- lib/screens/pdf_screen.dart
+++ lib/screens/pdf_screen.dart
@@ -184,36 +184,9 @@ class PdfScreen extends StatelessWidget {
 
   //Draws the grid
   void drawGrid(PdfPage page, PdfGrid grid, PdfLayoutResult result) {
-    Rect? totalPriceCellBounds;
-    Rect? quantityCellBounds;
-    //Invoke the beginCellLayout event.
-    grid.beginCellLayout = (Object sender, PdfGridBeginCellLayoutArgs args) {
-      final PdfGrid grid = sender as PdfGrid;
-      if (args.cellIndex == grid.columns.count - 1) {
-        totalPriceCellBounds = args.bounds;
-      } else if (args.cellIndex == grid.columns.count - 2) {
-        quantityCellBounds = args.bounds;
-      }
-    };
-    //Draw the PDF grid and get the result.
+    //Draw the PDF grid
     result = grid.draw(
         page: page, bounds: Rect.fromLTWH(0, result.bounds.bottom + 40, 0, 0))!;
-
-    //Draw grand total.
-    page.graphics.drawString('Grand Total',
-        PdfStandardFont(PdfFontFamily.helvetica, 9, style: PdfFontStyle.bold),
-        bounds: Rect.fromLTWH(
-            quantityCellBounds!.left,
-            result.bounds.bottom + 10,
-            quantityCellBounds!.width,
-            quantityCellBounds!.height));
-    page.graphics.drawString(getTotalAmount(grid).toString(),
-        PdfStandardFont(PdfFontFamily.helvetica, 9, style: PdfFontStyle.bold),
-        bounds: Rect.fromLTWH(
-            totalPriceCellBounds!.left,
-            result.bounds.bottom + 10,
-            totalPriceCellBounds!.width,
-            totalPriceCellBounds!.height));
   }
 
   //Draw the invoice footer data.
@@ -242,44 +215,24 @@ class PdfScreen extends StatelessWidget {
     //Create a PDF grid
     final PdfGrid grid = PdfGrid();
     //Secify the columns count to the grid.
-    grid.columns.add(count: 5);
-    //Create the header row of the grid.
-    final PdfGridRow headerRow = grid.headers.add(1)[0];
-    //Set style
-    headerRow.style.backgroundBrush = PdfSolidBrush(PdfColor(68, 114, 196));
-    headerRow.style.textBrush = PdfBrushes.white;
-    headerRow.cells[0].value = 'Product Id';
-    headerRow.cells[0].stringFormat.alignment = PdfTextAlignment.center;
-    headerRow.cells[1].value = 'Product Name';
-    headerRow.cells[2].value = 'Price';
-    headerRow.cells[3].value = 'Quantity';
-    headerRow.cells[4].value = 'Total';
-    //Add rows
-    addProducts('CA-1098', 'AWC Logo Cap', 8.99, 2, 17.98, grid);
-    addProducts('LJ-0192', 'Long-Sleeve Logo Jersey,M', 49.99, 3, 149.97, grid);
-    addProducts('So-B909-M', 'Mountain Bike Socks,M', 9.5, 2, 19, grid);
-    addProducts('LJ-0192', 'Long-Sleeve Logo Jersey,M', 49.99, 4, 199.96, grid);
-    addProducts('FK-5136', 'ML Fork', 175.49, 6, 1052.94, grid);
-    addProducts('HL-U509', 'Sports-100 Helmet,Black', 34.99, 1, 34.99, grid);
-    //Apply the table built-in style
-    grid.applyBuiltInStyle(PdfGridBuiltInStyle.listTable4Accent5);
-    //Set gird columns width
-    grid.columns[1].width = 200;
-    for (int i = 0; i < headerRow.cells.count; i++) {
-      headerRow.cells[i].style.cellPadding =
-          PdfPaddings(bottom: 5, left: 5, right: 5, top: 5);
-    }
-    for (int i = 0; i < grid.rows.count; i++) {
-      final PdfGridRow row = grid.rows[i];
-      for (int j = 0; j < row.cells.count; j++) {
-        final PdfGridCell cell = row.cells[j];
-        if (j == 0) {
-          cell.stringFormat.alignment = PdfTextAlignment.center;
-        }
-        cell.style.cellPadding =
-            PdfPaddings(bottom: 5, left: 5, right: 5, top: 5);
-      }
-    }
+    grid.columns.add(count: 7);
+
+    //Add header to the grid
+    grid.headers.add(2);
+
+    //Add the rows to the grid
+    PdfGridRow header = grid.headers[0];
+    header.cells[0].value = 'Item';
+    header.cells[1].value = 'Employee Name';
+    header.cells[2].value = 'Salary';
+
+    PdfGridRow header1 = grid.headers[1];
+    header1.cells[0].value = 'Item';
+    header1.cells[1].value = 'Employee Name';
+    header1.cells[1].columnSpan = 2;
+    // header1.cells[2].value = '';
+    header1.cells[3].value = 'Salary';
+
     return grid;
   }
 
