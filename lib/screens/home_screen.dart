e54973849c3e462b0d221b0a9885c125a4e9132a
Elver Rodriguez <adelson9602@gmail.com>
2022-05-05 15:40:31 -0500

    se refactoriza codigo, se convierte stateless a statefull el widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,24 +3,29 @@ import 'package:provider/provider.dart';
 
 import 'package:app_qinspecting/providers/providers.dart';
 import 'package:app_qinspecting/screens/screens.dart';
+import 'package:app_qinspecting/services/services.dart';
 import 'package:app_qinspecting/widgets/widgets.dart';
 
-class LoadHomeScreen extends StatelessWidget {
-  const LoadHomeScreen({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return const HomeScreen();
-  }
-}
-
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final uiProvider = Provider.of<UiProvider>(context);
-    final inspeccionProvider =
-        Provider.of<InspeccionProvider>(context, listen: false);
+    final inspeccionProvider = Provider.of<InspeccionProvider>(context, listen: false);
+    final loginService = Provider.of<LoginService>(context, listen: false);
 
     List<Widget> _widgetOptions = [
       DesktopScreen(),
@@ -28,7 +33,7 @@ class HomeScreen extends StatelessWidget {
           future: inspeccionProvider.listarDataInit(),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
-              return const LoadingScreen();
+              return LoadingScreen();
             } else {
               return InspeccionForm();
             }
@@ -36,14 +41,34 @@ class HomeScreen extends StatelessWidget {
     ];
 
     return Scaffold(
-      appBar: const CustomAppBar().createAppBar(context),
-      drawer: const CustomDrawer(),
+      appBar: CustomAppBar(),
+      drawer: CustomDrawer(),
       body: SafeArea(
           child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 20),
-        child: _widgetOptions.elementAt(uiProvider.selectedMenuOpt),
+        child: _widgetOptions.elementAt(_selectedIndex),
       )),
-      bottomNavigationBar: CustomNavigationBar(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Escritorio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.app_registration_sharp),
+            label: 'Inspecciones',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.green,
+        onTap: (int index) {
+          if (index == 1 && loginService.userDataLogged.firmaId == 0) {
+            Navigator.popAndPushNamed(context, 'signature');
+          } else {
+            _onItemTapped(index);
+          }
+        },
+      ),
     );
   }
 }
