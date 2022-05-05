cb63d4569b2444fe751aec3fe0d965c60794de4e
Elver Rodriguez <adelson9602@gmail.com>
2022-05-05 15:33:40 -0500

    Se elimina importación de widget y se elimina widget

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,7 +1,6 @@
 export 'package:app_qinspecting/widgets/auth_background.dart';
 export 'package:app_qinspecting/widgets/custom_app_bar.dart';
 export 'package:app_qinspecting/widgets/custom_drawer.dart';
-export 'package:app_qinspecting/widgets/custom_navigatorbar.dart';
 export 'package:app_qinspecting/widgets/board_image.dart';
 export 'package:app_qinspecting/widgets/terminos_condiciones.dart';
 export 'package:app_qinspecting/widgets/items_inspeccion_vehiculo.dart';
