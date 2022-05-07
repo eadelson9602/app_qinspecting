f37bdf4907fdf2cdabda76c3d9c89aab7ae2d339
Elver Rodriguez <adelson9602@gmail.com>
2022-05-06 22:13:47 -0500

    se muestran datos de la inspección offline en el pdf

--- lib/screens/pdf_screen_offline.dart
+++ lib/screens/pdf_screen_offline.dart
@@ -1,17 +1,17 @@
 // import 'package:printing/printing.dart';
 import 'dart:convert';
 import 'dart:io';
-import 'dart:typed_data';
-// import 'package:app_qinspecting/providers/inspeccion_provider.dart';
-import 'package:app_qinspecting/screens/screens.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:provider/provider.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:syncfusion_flutter_pdf/pdf.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 import 'package:app_qinspecting/models/models.dart';
-// import 'package:provider/provider.dart';
+import 'package:app_qinspecting/providers/providers.dart';
+import 'package:app_qinspecting/screens/screens.dart';
+import 'package:app_qinspecting/services/services.dart';
 
 class PdfScreenOffline extends StatelessWidget {
   const PdfScreenOffline({Key? key}) : super(key: key);
@@ -20,44 +20,57 @@ class PdfScreenOffline extends StatelessWidget {
   Widget build(BuildContext context) {
     final params = ModalRoute.of(context)!.settings.arguments as List;
     final resumenPreoperacional = params[0] as ResumenPreoperacional;
-    // final inspeccionProvider = Provider.of<InspeccionProvider>(context, listen: false);
+    final loginService = Provider.of<LoginService>(context, listen: false);
     return FutureBuilder(
-      future: _generatePdf(resumenPreoperacional),
-      builder: (context, snapshot) {
-        if (snapshot.data == null) {
-          return LoadingScreen();
-        } else {
-          var tempFile = snapshot.data as File;
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Preoperacional ${resumenPreoperacional.id}'),
-            ),
-            body: SfPdfViewer.file(tempFile),
-            floatingActionButton: FloatingActionButton(
-              onPressed: () async {
-                await Share.shareFiles([tempFile.path]);
-              },
-              tooltip: 'Compartir',
-              child: const Icon(Icons.share),
-            ),
-          );
-        }
-      }
-    );
+        future: _generatePdf(resumenPreoperacional, loginService),
+        builder: (context, snapshot) {
+          if (snapshot.data == null) {
+            return LoadingScreen();
+          } else {
+            var tempFile = snapshot.data as File;
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('Preoperacional ${resumenPreoperacional.id}'),
+              ),
+              body: Padding(
+                padding: EdgeInsets.only(bottom: 30),
+                child: SfPdfViewer.file(tempFile),
+              ),
+              floatingActionButton: FloatingActionButton(
+                onPressed: () async {
+                  await Share.shareFiles([tempFile.path]);
+                },
+                tooltip: 'Compartir',
+                child: const Icon(Icons.share),
+              ),
+            );
+          }
+        });
   }
 
   // Genera el pdf
-  Future<File> _generatePdf(ResumenPreoperacional resumenPreoperacional) async {
-    List<Detalle> detalleInspeccion;
-    
-    // Mapeamos las respuestas
-    var respuestas = jsonDecode(resumenPreoperacional.respuestas!);
-    print(respuestas);
-    
+  Future<File> _generatePdf(
+      ResumenPreoperacional infoPdf, LoginService loginService) async {
+    // Directory documentsDirectory = await getApplicationDocumentsDirectory();
 
-    // var logoCliente = PdfBitmap(File(resumenPreoperacional.resuPreFotokm!).readAsBytesSync());
-    // var logoQi = PdfBitmap(File(resumenPreoperacional.resuPreFotokm!).readAsBytesSync());
-    // var fotoKilometraje = PdfBitmap(File(resumenPreoperacional.resuPreFotokm!).readAsBytesSync());
+    // Mapeamos las respuestas a una lista
+    List<ItemsVehiculo> respuestas = [];
+    List tempData = jsonDecode(infoPdf.respuestas!) as List;
+    tempData.forEach((element) {
+      // Tranformamor el elemento a una instancia de ItemsVehiculo
+      final data = ItemsVehiculo.fromMap(element);
+      // Filtramos los items que tienen respuesta
+      final tempRespuestas = data.items.where((item) => item.respuesta != null).toList();
+      // Si hay respuestas los pusheamos al array de respuestas
+      if(tempRespuestas.length > 0){
+        respuestas.add(data);
+      }
+    });
+
+    final infoVehiculo = await DBProvider.db.getVehiculoByPlate(infoPdf.placa!);
+
+    var fotoKilometraje =
+        PdfBitmap(File(infoPdf.resuPreFotokm!).readAsBytesSync());
 
     // var firmaConductor = PdfBitmap(File(resumenPreoperacional.resuPreFotokm!).readAsBytesSync());
     // var firmaAuditor = PdfBitmap(File(resumenPreoperacional.resuPreFotokm!).readAsBytesSync());
@@ -70,34 +83,29 @@ class PdfScreenOffline extends StatelessWidget {
     final Size pageSize = page.getClientSize();
     //Draw rectangle used how margin
     page.graphics.drawRectangle(
-      bounds: Rect.fromLTWH(0, 0, pageSize.width, pageSize.height),
-      pen: PdfPen(PdfColor(0, 0, 0))
-    );
-
-    // Draw header
-    // final header = drawHeader(document, infoPdf, logoCliente, logoQi);
-    // document.template.top = header;
+        bounds: Rect.fromLTWH(0, 0, pageSize.width, pageSize.height),
+        pen: PdfPen(PdfColor(0, 0, 0)));
 
     //Generate PDF grid.
-    // final PdfGrid gridSummary = getGridSummary(infoPdf, pageSize, firmaConductor, firmaAuditor, resumenPreoperacional);
+    final PdfGrid gridSummary =
+        getGridSummary(infoPdf, pageSize, infoVehiculo!, loginService);
 
-    // final PdfGrid gridAnswers = getGridAnswers(infoPdf, pageSize, fotoKilometraje);
+    final PdfGrid gridAnswers =
+        getGridAnswers(infoPdf, pageSize, fotoKilometraje, respuestas);
 
     //Draw grid
-    // PdfLayoutResult resultSummary = gridSummary.draw(
-    //   page: page, bounds: Rect.fromLTWH(0, 60, 0, 0)
-    // ) as PdfLayoutResult;
+    PdfLayoutResult resultSummary = gridSummary.draw(
+        page: page, bounds: Rect.fromLTWH(0, 0, 0, 0)) as PdfLayoutResult;
 
     //Draw the PDF grid
-    // gridAnswers.draw(
-    //   page: page,
-    //   bounds: Rect.fromLTWH(0, resultSummary.bounds.bottom, 0, 0)
-    // );
+    gridAnswers.draw(
+        page: page,
+        bounds: Rect.fromLTWH(0, resultSummary.bounds.bottom, 0, 0));
 
     //Save the PDF document
     final output = await getTemporaryDirectory();
-    print(output.path);
-    final pathFile = '${output.path}/${resumenPreoperacional.id}.pdf';
+    // print(output.path);
+    final pathFile = '${output.path}/${infoPdf.id}.pdf';
 
     File(pathFile).writeAsBytes(document.save());
     // Dispose the document.
@@ -106,8 +114,8 @@ class PdfScreenOffline extends StatelessWidget {
     return File(pathFile);
   }
 
-  PdfPageTemplateElement drawHeader(PdfDocument document, Pdf infoPdf,
-      Uint8List logoCliente, Uint8List logoQi) {
+  PdfPageTemplateElement drawHeader(PdfDocument document,
+      ResumenPreoperacional infoPdf, PdfBitmap logoCliente, PdfBitmap logoQi) {
     final pageSize = document.pages[0].getClientSize();
     //Create the header with specific bounds
     PdfPageTemplateElement header =
@@ -118,8 +126,7 @@ class PdfScreenOffline extends StatelessWidget {
         bounds: Rect.fromLTWH(0, 0, 120, 60), pen: PdfPen(PdfColor(0, 0, 0)));
 
     //Dibuja el logo del cliente
-    header.graphics
-        .drawImage(PdfBitmap(logoCliente), Rect.fromLTWH(1, 1, 118, 57));
+    header.graphics.drawImage(logoCliente, Rect.fromLTWH(1, 1, 118, 57));
 
     //Diduja el rectangulo con el titulo del pdf
     header.graphics.drawRectangle(
@@ -127,7 +134,7 @@ class PdfScreenOffline extends StatelessWidget {
         pen: PdfPen(PdfColor(0, 0, 0)));
 
     //Dibuja el título del pdf
-    header.graphics.drawString('${infoPdf.nombreFormatoPreope}',
+    header.graphics.drawString('INSPECCIÓN DE VEHÍCULOS DE CARGA',
         PdfStandardFont(PdfFontFamily.helvetica, 10),
         brush: PdfBrushes.black,
         bounds: Rect.fromLTWH(122, 0, pageSize.width - 285, 60),
@@ -151,8 +158,8 @@ class PdfScreenOffline extends StatelessWidget {
     header.graphics.drawRectangle(
         bounds: Rect.fromLTWH(405, 0, 50, 30), pen: PdfPen(PdfColor(0, 0, 0)));
     //Texto para CODIGO DE LA BASE
-    header.graphics.drawString('${infoPdf.codFormtPreope}',
-        PdfStandardFont(PdfFontFamily.helvetica, 9),
+    header.graphics.drawString(
+        'CODE', PdfStandardFont(PdfFontFamily.helvetica, 9),
         brush: PdfBrushes.black,
         bounds: Rect.fromLTWH(405, 0, 50, 30),
         format: PdfStringFormat(
@@ -175,8 +182,7 @@ class PdfScreenOffline extends StatelessWidget {
     header.graphics.drawRectangle(
         bounds: Rect.fromLTWH(405, 30, 50, 30), pen: PdfPen(PdfColor(0, 0, 0)));
     //Texto para VERSION DE LA BASE
-    header.graphics.drawString('${infoPdf.versionFormtPreope}',
-        PdfStandardFont(PdfFontFamily.helvetica, 9),
+    header.graphics.drawString('V', PdfStandardFont(PdfFontFamily.helvetica, 9),
         brush: PdfBrushes.black,
         bounds: Rect.fromLTWH(405, 30, 50, 30),
         format: PdfStringFormat(
@@ -188,20 +194,14 @@ class PdfScreenOffline extends StatelessWidget {
         bounds: Rect.fromLTWH(455, 0, 60, 60), pen: PdfPen(PdfColor(0, 0, 0)));
 
     //Dibuja el logo de QI
-    header.graphics.drawImage(PdfBitmap(logoQi), Rect.fromLTWH(462, 8, 45, 45));
+    header.graphics.drawImage(logoQi, Rect.fromLTWH(462, 8, 45, 45));
 
     //Return header
     return header;
   }
 
-  PdfGrid getGridSummary(
-    Pdf infoPdf,
-    Size pageSize,
-    firmaConductor,
-    firmaAuditor,
-    ResumenPreoperacional
-    resumenPreoperacional
-  ) {
+  PdfGrid getGridSummary(ResumenPreoperacional infoPdf, Size pageSize,
+      Vehiculo infoVehiculo, LoginService loginService) {
     //Create a PDF grid
     final PdfGrid gridSummary = PdfGrid();
     //Secify the columns count to the grid.
@@ -209,41 +209,42 @@ class PdfScreenOffline extends StatelessWidget {
 
     //Add header to the grid
     gridSummary.headers.add(3);
-    gridSummary.style = PdfGridStyle(cellPadding: PdfPaddings(left: 5, right: 5, top: 0, bottom: 0));
+    gridSummary.style = PdfGridStyle(
+        cellPadding: PdfPaddings(left: 5, right: 5, top: 0, bottom: 0));
 
     PdfGridRow rowSummary = gridSummary.headers[0];
     rowSummary.cells[0].value = '''CIUDAD Y FECHA: 
-    ${infoPdf.resuPreFecha}''';
+    ${infoPdf.ciudad} / ${infoPdf.resuPreFecha}''';
     rowSummary.cells[0].columnSpan = 2;
     rowSummary.cells[2].value = '''TIPO DE VEHÍCULO:
-    ${infoPdf.tvDescripcion}''';
+    PENDIENTE''';
     rowSummary.cells[3].value = '''MARCA/LÍNEA/MODELO:
-    ${infoPdf.mlm}''';
+    ${infoVehiculo.marca} / ${infoVehiculo.modelo}''';
     rowSummary.cells[3].columnSpan = 2;
     rowSummary.cells[5].value = '''¿TANQUEO?
-    ${infoPdf.tanque}''';
+    ${infoPdf.tanqueGalones != null || infoPdf.tanqueGalones != 0 ? 'SI' : 'NO'}''';
 
     PdfGridRow rowSummary1 = gridSummary.headers[1];
     rowSummary1.cells[0].value = '''KILOMETRAJE:
-    ${infoPdf.kilometraje}''';
+    ${infoPdf.resuPreKilometraje}''';
     rowSummary1.cells[1].value = '''NOMBRE QUIEN REALIZÓ LA INSPECCIÓN:
-    ${infoPdf.conductor}''';
+    ${loginService.userDataLogged.persNombres}''';
     rowSummary1.cells[1].columnSpan = 2;
     rowSummary1.cells[3].value = '''PLACA VEHÍCULO:
-    ${infoPdf.vehPlaca}''';
+    ${infoPdf.placa}''';
     rowSummary1.cells[4].value = '''PLACA REMOLQUE:
-    ${infoPdf.remolPlaca}''';
+    ${infoPdf.remolquePlaca != null ? infoPdf.remolquePlaca : ''}''';
     rowSummary1.cells[5].value = '''ESTADO:
     PENDIENTE''';
 
     PdfGridRow rowSummary2 = gridSummary.headers[2];
     rowSummary2.cells[0].value = 'N°. INSPECCIÓN';
-    rowSummary2.cells[1].value = '${infoPdf.consecutivo}';
+    rowSummary2.cells[1].value = '${infoPdf.id}';
     rowSummary2.cells[2].value = 'FIRMA CONDUCTOR';
     // rowSummary2.cells[3].style = PdfGridCellStyle(backgroundImage: PdfBitmap(firmaConductor));
     rowSummary2.height = 30;
     rowSummary2.cells[4].value = 'FIRMA DE QUIEN INSPECCIONA';
-    rowSummary2.cells[5].value = 'FOTO DE LA FIRMA';
+    // rowSummary2.cells[5].value = 'FOTO DE LA FIRMA';
 
     // Styles for table
     gridSummary.style = PdfGridStyle(
@@ -273,8 +274,8 @@ class PdfScreenOffline extends StatelessWidget {
   }
 
   //Create PDF grid and return
-  PdfGrid getGridAnswers(
-      Pdf infoPdf, Size pageSize, Uint8List fotoKilometraje) {
+  PdfGrid getGridAnswers(ResumenPreoperacional infoPdf, Size pageSize,
+      PdfBitmap fotoKilometraje, List<ItemsVehiculo> respuestas) {
     //Create a PDF grid
     final PdfGrid grid = PdfGrid();
     //Secify the columns count to the grid.
@@ -332,21 +333,21 @@ class PdfScreenOffline extends StatelessWidget {
 
     DateTime fechaHoy = DateTime.now();
     //Add rows
-    infoPdf.detalle.last.respuestas.add(RespuestaInspeccion(
-        idItem: -1,
-        item: 'Kilometraje',
-        foto: infoPdf.fotoKm,
-        fotoConverted: fotoKilometraje));
+    respuestas.last.items.add(Item(
+      idItem: '-1',
+      item: 'Kilometraje',
+      adjunto: infoPdf.resuPreFotokm,
+    ));
 
-    infoPdf.detalle.forEach((categoria) {
+    respuestas.forEach((element) {
       // Dibujas las categorias
       final PdfGridRow row = grid.rows.add();
-      row.cells[0].value = categoria.categoria;
+      row.cells[0].value = element.categoria;
       row.cells[0].columnSpan = 7;
       row.cells[0].style =
           PdfGridCellStyle(backgroundBrush: PdfBrushes.lightGray);
       // Dibuja los items
-      categoria.respuestas.forEach((respuesta) {
+      element.items.forEach((respuesta) {
         addRows(grid, infoPdf, respuesta, formatColumns, fechaHoy);
       });
     });
@@ -354,7 +355,7 @@ class PdfScreenOffline extends StatelessWidget {
   }
 
   //Create and row for the grid.
-  void addRows(PdfGrid grid, Pdf infoPdf, RespuestaInspeccion respuesta,
+  void addRows(PdfGrid grid, ResumenPreoperacional infoPdf, Item respuesta,
       PdfStringFormat formatColumns, DateTime fechaHoy) async {
     final PdfGridRow row = grid.rows.add();
     row.cells[0].value = '${respuesta.item}';
@@ -371,76 +372,21 @@ class PdfScreenOffline extends StatelessWidget {
     }
 
     // Une las celdas de respuestas S, N, B, M y Observaciones para mostrar el kilometraje al final del pdf
-    if (respuesta.idItem == -1) {
+    if (respuesta.idItem == '-1') {
       row.cells[1].columnSpan = 5;
-      row.cells[1].value = '${infoPdf.kilometraje} KM';
+      row.cells[1].value = '${infoPdf.resuPreKilometraje} KM';
       row.cells[1].style.stringFormat = formatColumns;
     }
-
     row.cells[5].style.stringFormat = formatColumns;
+    row.cells[5].value =
+        '${respuesta.observaciones == null ? '' : respuesta.observaciones}';
 
-    if (respuesta.idItem == 1 && infoPdf.fechaVencLicCond != '') {
-      // 1, "Licencia de Tránsito"
-      row.cells[5].value = 'Fecha de Vencimiento: ${infoPdf.fechaVencLicCond}';
-
-      DateTime tempDate = DateTime.parse(infoPdf.fechaVencLicCond!);
-      final difference = tempDate.difference(fechaHoy).inDays;
-      row.cells[5].style.backgroundBrush = difference <= 15 && difference > 0
-          ? PdfBrushes.orange
-          : PdfBrushes.red;
-    } else if (respuesta.idItem == 3 && infoPdf.fechaFinSoat != '') {
-      //3, "Soat"
-      row.cells[5].value = 'Fecha de Vencimiento: ${infoPdf.fechaFinSoat}';
-
-      DateTime tempDate = DateTime.parse(infoPdf.fechaFinSoat!);
-      final difference = tempDate.difference(fechaHoy).inDays;
-      row.cells[5].style.backgroundBrush = difference <= 15 && difference > 0
-          ? PdfBrushes.orange
-          : PdfBrushes.red;
-    } else if (respuesta.idItem == 4 && infoPdf.fechaFinPoExtra != '') {
-      //4, "Póliza Contra Actual"
-      row.cells[5].value = 'Fecha de Vencimiento: ${infoPdf.fechaFinPoExtra}';
-
-      DateTime tempDate = DateTime.parse(infoPdf.fechaFinPoExtra!);
-      final difference = tempDate.difference(fechaHoy).inDays;
-      row.cells[5].style.backgroundBrush = difference <= 15 && difference > 0
-          ? PdfBrushes.orange
-          : PdfBrushes.red;
-    } else if (respuesta.idItem == 5 && infoPdf.rcHidroFechaFin != '') {
-      //5, "Póliza Extracontractual (Hidrocarburos)"
-      row.cells[5].value = 'Fecha de Vencimiento: ${infoPdf.rcHidroFechaFin}';
-
-      DateTime tempDate = DateTime.parse(infoPdf.rcHidroFechaFin!);
-      final difference = tempDate.difference(fechaHoy).inDays;
-      row.cells[5].style.backgroundBrush = difference <= 15 && difference > 0
-          ? PdfBrushes.orange
-          : PdfBrushes.red;
-    } else if (respuesta.idItem == 6 && infoPdf.fechaFinReTec != '') {
-      //6, "Certificado de Revisión Técnico mecánica  y de Gases"
-      row.cells[5].value = 'Fecha de Vencimiento: ${infoPdf.fechaFinReTec}';
-
-      DateTime tempDate = DateTime.parse(infoPdf.fechaFinReTec!);
-      final difference = tempDate.difference(fechaHoy).inDays;
-      row.cells[5].style.backgroundBrush = difference <= 15 && difference > 0
-          ? PdfBrushes.orange
-          : PdfBrushes.red;
-    } else if (respuesta.idItem == 7 && infoPdf.fechaFinQr != '') {
-      //7, "Revisión Luz Negra 5° Rueda"
-      row.cells[5].value = 'Fecha de Vencimiento: ${infoPdf.fechaFinQr}';
-
-      DateTime tempDate = DateTime.parse(infoPdf.fechaFinQr!);
-      final difference = tempDate.difference(fechaHoy).inDays;
-      row.cells[5].style.backgroundBrush = difference <= 15 && difference > 0
-          ? PdfBrushes.orange
-          : PdfBrushes.red;
-    } else {
-      row.cells[5].value =
-          '${respuesta.observacion == null ? '' : respuesta.observacion}';
-    }
-    if (respuesta.foto == null) {
+    if (respuesta.adjunto == null) {
       row.cells[6].value = '';
     } else {
-      row.cells[6].style = PdfGridCellStyle(backgroundImage: PdfBitmap(respuesta.fotoConverted!));
+      row.cells[6].style = PdfGridCellStyle(
+          backgroundImage:
+              PdfBitmap(File(respuesta.adjunto!).readAsBytesSync()));
       row.height = 40;
     }
   }
