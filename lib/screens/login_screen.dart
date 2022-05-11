89a43e048e2fa7c7c1645b384fb731d75feb4ea9
Elver Rodriguez <adelson9602@gmail.com>
2022-05-11 15:56:01 -0500

    Se agrega remember screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -29,9 +29,7 @@ class LoginScreen extends StatelessWidget {
               child: const _FormLogin(),
             ),
           ),
-          Center(
-            child: Text('Second Page'),
-          ),
+          RememberPasswordScreen(),
         ],
       ),
     );
