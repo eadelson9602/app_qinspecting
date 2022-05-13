ebb57703060e2f6e5450e4b99f82f5cf587921a0
Elver Rodriguez <adelson9602@gmail.com>
2022-05-12 21:43:11 -0500

    Se agrega options a las peticiones para enviar el token

--- lib/services/firma_service.dart
+++ lib/services/firma_service.dart
@@ -8,6 +8,12 @@ class FirmaService extends ChangeNotifier {
   bool isLoading = false;
   int indexTabaCreateSignature = 0;
   String aceptaTerminos = 'NO';
+  String baseUrl = 'https://apis.qinspecting.com/pflutter';
+  Options options = Options(
+    headers: {
+      'x-access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIzNDU2Nzg5LCJpYXQiOjE2NTI0MDg5NTcsImV4cCI6MTY1MjQ5NTM1N30.ufyCwGVWC9x6vmYusL-9GhgrabBlbM3rDuWw98wnHe0'
+    }
+  );
 
   updateTabIndex(int value) {
     indexTabaCreateSignature = value;
@@ -22,17 +28,14 @@ class FirmaService extends ChangeNotifier {
   Future<Map<dynamic, dynamic>> insertSignature(Map firma) async {
     isLoading = true;
     notifyListeners();
-    Response response = await dio.post(
-        'https://apis.qinspecting.com/pflutter/insert_signature',
-        data: jsonEncode(firma));
+    Response response = await dio.post('${baseUrl}/insert_signature', options: options, data: jsonEncode(firma));
     isLoading = false;
     notifyListeners();
     return response.data;
   }
 
   Future<Firma?> getInfoFirma(Empresa empresaSelected) async {
-    Response response = await dio.get(
-        'https://apis.qinspecting.com/pflutter/get_info_firma/${empresaSelected.nombreBase}/${empresaSelected.usuarioUser}');
+    Response response = await dio.get('${baseUrl}/get_info_firma/${empresaSelected.nombreBase}/${empresaSelected.usuarioUser}', options: options);
 
     return response.data.toString().isNotEmpty
         ? Firma.fromMap(response.data)
