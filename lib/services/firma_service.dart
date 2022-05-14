f2403f6a20f46018659684d300a2dbfc0131982e
Elver Rodriguez <adelson9602@gmail.com>
2022-05-13 23:01:30 -0500

    Se agraga instancia de servicio login, se agrega try catch, se agrega asignación de token

--- lib/services/firma_service.dart
+++ lib/services/firma_service.dart
@@ -2,18 +2,17 @@ import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:app_qinspecting/models/models.dart';
+import 'package:app_qinspecting/services/services.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:overlay_support/overlay_support.dart';
 
 class FirmaService extends ChangeNotifier {
-  var dio = Dio();
+  final loginService = LoginService();
+  final dio = Dio();
   bool isLoading = false;
   int indexTabaCreateSignature = 0;
   String aceptaTerminos = 'NO';
-  String baseUrl = 'https://apis.qinspecting.com/pflutter';
-  Options options = Options(
-    headers: {
-      'x-access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIzNDU2Nzg5LCJpYXQiOjE2NTI0MDg5NTcsImV4cCI6MTY1MjQ5NTM1N30.ufyCwGVWC9x6vmYusL-9GhgrabBlbM3rDuWw98wnHe0'
-    }
-  );
+  final storage = new FlutterSecureStorage();
 
   updateTabIndex(int value) {
     indexTabaCreateSignature = value;
@@ -26,19 +25,47 @@ class FirmaService extends ChangeNotifier {
   }
 
   Future<Map<dynamic, dynamic>> insertSignature(Map firma) async {
-    isLoading = true;
-    notifyListeners();
-    Response response = await dio.post('${baseUrl}/insert_signature', options: options, data: jsonEncode(firma));
-    isLoading = false;
-    notifyListeners();
-    return response.data;
+    try {
+      isLoading = true;
+      notifyListeners();
+
+      Response response = await dio.post('${loginService.baseUrl}/insert_signature', options: loginService.options, data: jsonEncode(firma));
+
+      isLoading = false;
+      notifyListeners();
+      return response.data;
+    } on DioError catch (error) {
+      print(error.response!.data);
+      showSimpleNotification(
+        Text('No hemos podido obtener la firma'),
+        leading: Icon(Icons.wifi_tethering_error_rounded_outlined),
+        autoDismiss: true,
+        background: Colors.orange,
+        position: NotificationPosition.bottom,
+      );
+      return Future.error(error.response!.data);
+    }
   }
 
   Future<Firma?> getInfoFirma(Empresa empresaSelected) async {
-    Response response = await dio.get('${baseUrl}/get_info_firma/${empresaSelected.nombreBase}/${empresaSelected.usuarioUser}', options: options);
+    try {
+      // Buscamos en el storage el token y lo asignamos a la instancia para poderlo usar en todas las peticiones de este servicio
+      String token = await storage.read(key: 'token') ?? '';
+      loginService.options.headers = {
+        "x-access-token": token
+      };
+      Response response = await dio.get('${loginService.baseUrl}/get_info_firma/${empresaSelected.nombreBase}/${empresaSelected.usuarioUser}', options: loginService.options);
 
-    return response.data.toString().isNotEmpty
-        ? Firma.fromMap(response.data)
-        : null;
+      return response.data.toString().isNotEmpty ? Firma.fromMap(response.data) : null;
+    } on DioError catch (error) {
+      showSimpleNotification(
+        Text('No hemos podido obtener la firma'),
+        leading: Icon(Icons.wifi_tethering_error_rounded_outlined),
+        autoDismiss: true,
+        background: Colors.orange,
+        position: NotificationPosition.bottom,
+      );
+      return Future.error(error.response!.data);
+    }
   }
 }
