1fcce394b50c0515178d8d492fa9cf61c8dd4d89
Elver Rodriguez <adelson9602@gmail.com>
2022-05-14 13:58:45 -0500

    Se agrega future para validar conexión a internet

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'package:app_qinspecting/services/login_service.dart';
+import 'package:app_qinspecting/services/services.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CustomAppBar({Key? key}) : super(key: key);
@@ -10,6 +10,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     final loginService = Provider.of<LoginService>(context, listen: false);
+    final inspeccionService = Provider.of<InspeccionService>(context, listen: false);
     String url = loginService.userDataLogged.persImagen;
     return AppBar(
       title: const Text('Qinspecting'),
@@ -20,11 +21,19 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
           child: CircleAvatar(
             child: ClipRRect(
               borderRadius: BorderRadius.all(Radius.circular(25)),
-              child: FadeInImage(
-                placeholder: const AssetImage('assets/images/loading-2.gif'),
-                image: NetworkImage(url.toString()),
-                fit: BoxFit.cover,
-                height: 40,
+              child: FutureBuilder(
+                future: inspeccionService.checkConnection(),
+                builder: (context, snapshot) {
+                  if(snapshot.data == true){
+                    return FadeInImage(
+                      placeholder: const AssetImage('assets/images/loading-2.gif'),
+                      image: NetworkImage(url.toString()),
+                      fit: BoxFit.cover,
+                      height: 40
+                    );
+                  }
+                  return Image(image: AssetImage('assets/images/loading-2.gif'));
+                },
               ),
             ),
           ),
