cb2d269fb13018a362856a4ed5cb51fc894c3b68
eadelson9602 <eadelson1606@hotmail.com>
2025-10-21 19:47:55 -0500

    fix: Agregar import faltante de Firebase Crashlytics en QuickCrashlyticsTest
    - Corregir error "Undefined name 'FirebaseCrashlytics'"
    - Agregar import 'package:firebase_crashlytics/firebase_crashlytics.dart'
    - Permitir uso del widget de prueba rápida sin errores de compilación

--- lib/widgets/quick_crashlytics_test.dart
+++ lib/widgets/quick_crashlytics_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:app_qinspecting/services/services.dart';
 
 class QuickCrashlyticsTest extends StatelessWidget {
