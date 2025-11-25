4a745597720b653c067677d45b5293e0fe9deac9
Elver Rodriguez <eadelson1606@hotmail.com>
2025-11-25 13:07:04 -0500

    feat(android): enable edge-to-edge and bump version

--- android/app/src/main/kotlin/com/qinspecting/mobile/MainActivity.kt
+++ android/app/src/main/kotlin/com/qinspecting/mobile/MainActivity.kt
@@ -1,14 +1,33 @@
 package com.qinspecting.mobile
 
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.SystemBarStyle
+import androidx.activity.enableEdgeToEdge
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.auto(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.auto(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            )
+        )
+    }
+
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         GeneratedPluginRegistrant.registerWith(flutterEngine)
-        
+
         // Registrar plugin personalizado para el servicio de primer plano
         flutterEngine.plugins.add(UploadServicePlugin())
     }
